e8ceacc00be51995d7e27050969121badae34261
IshitaAwadhiya <ishitaawadhiya@gmail.com>
2024-05-14 08:13:14 +0530

    source code

---
+++ strf_time.py
@@ -0,0 +1,24 @@
+'''from datetime import datetime
+
+now = datetime.now() # current date and time
+
+year = now.strftime("%Y")
+print("year:", year)
+
+month = now.strftime("%m")
+print("month:", month)
+
+day = now.strftime("%d")
+print("day:", day)
+
+time = now.strftime("%H:%M:%S")
+print("time:", time)
+
+date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+print("date and time:",date_time)	'''
+
+import calendar
+
+yy = 2024
+mm = 6
+print(calendar.month(yy,mm))
